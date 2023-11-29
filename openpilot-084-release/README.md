fa3310be6820379fcd91627802d8966026b526dc
Harrypark58 <harrypark58@gmail.com>
2023-11-29 14:21:10 +0900

    openpilot_084c2

---
+++ openpilot-084-release/README.md
@@ -0,0 +1,35 @@
+## @hanabi95님의 코드를 기반으로 하고있습니다. 항상 감사드립니다.
+
+## 일반 사용자분들은 "release" 브랜치를 사용하세요. 버전이 올라가도 최신을 그곳에 유지하겠습니다.
+
+## 콤마 순정 084 기반 작업물입니다.
+
+
+- 볼트EV 대상 한국어 오픈파일럿 포크입니다.
+- hanabi95(@hanabi95) 님의 작업을 기본으로 하고 있습니다. 항상 감사드립니다.
+- 블랙순정판다, Lepro, 볼트EV 2019 프리미터 세이프티 차량에서 공기압 계기판 기준 250으로  타고있습니다.
+
+문의사항은 오카방 혹은 이슈 생성으로 주십시오. 감사합니다.
+
+rkjnice@gmail.com
+
+
+
+This is a fork of comma's openpilot: https://github.com/commaai/openpilot, and contains tweaks for Hondas and GM vehicles.  It is open source and inherits MIT license.  By installing this software you accept all responsibility for anything that might occur while you use it.  All contributors to this fork are not liable, including me.  <b>Use at your own risk.</b>
+
+
+###for record purpose, beware mereging upstream, check those files.
+* controls.hpp	Conflicted (Both modified)	selfdrive/ui/qt/widgets
+* paint.cc	Conflicted (Both modified)	selfdrive/ui
+* ui.cc	Conflicted (Both modified)	selfdrive/ui
+* ui.hpp	Conflicted (Both modified)	selfdrive/ui
+* latcontrol_pid.py	Conflicted (Both modified)	selfdrive/controls/lib
+* lateral_planner.py	Conflicted (Both modified)	selfdrive/controls/lib
+* controlsd.py	Conflicted (Both modified)	selfdrive/controls
+* interface.py	Conflicted (Both modified)	selfdrive/car/gm
+* values.py	Conflicted (Both modified)	selfdrive/car/gm
+* interfaces.py	Conflicted (Both modified)	selfdrive/car
+* boardd.cc	Conflicted (Both modified)	selfdrive/boardd
+* pigeon.cc	Conflicted (Both modified)	selfdrive/boardd
+* SConstruct	Conflicted (Both modified)
+* latcontrol_lqr.py	Staged	selfdrive/controls/lib
---
+++ openpilot-084-release/selfdrive/car/gm/carcontroller.py
@@ -0,0 +1,105 @@
+from cereal import car
+from common.realtime import DT_CTRL
+from common.numpy_fast import interp, clip
+from selfdrive.config import Conversions as CV
+from selfdrive.car import apply_std_steer_torque_limits, create_gas_command
+from selfdrive.car.gm import gmcan
+from selfdrive.car.gm.values import DBC, CanBus, CarControllerParams
+from opendbc.can.packer import CANPacker
+
+VisualAlert = car.CarControl.HUDControl.VisualAlert
+ACCEL_HYST_GAP = 0.008
+
+def accel_hysteresis(accel, accel_steady):
+
+  # for small accel oscillations within ACCEL_HYST_GAP, don't change the accel command
+  if accel == 0:
+    accel_steady = 0.
+  elif accel > accel_steady + ACCEL_HYST_GAP:
+    accel_steady = accel - ACCEL_HYST_GAP
+  elif accel < accel_steady - ACCEL_HYST_GAP:
+    accel_steady = accel + ACCEL_HYST_GAP
+  accel = accel_steady
+
+  return accel, accel_steady
+
+class CarController():
+  def __init__(self, dbc_name, CP, VM):
+    self.start_time = 0.
+    self.apply_steer_last = 0
+    self.lka_icon_status_last = (False, False)
+    self.steer_rate_limited = False
+    self.accel_steady = 0.
+    self.apply_pedal_last = 0.
+
+    self.params = CarControllerParams()
+
+    self.packer_pt = CANPacker(DBC[CP.carFingerprint]['pt'])
+    self.packer_obj = CANPacker(DBC[CP.carFingerprint]['radar'])
+    self.packer_ch = CANPacker(DBC[CP.carFingerprint]['chassis'])
+
+  def update(self, enabled, CS, frame, actuators,
+             hud_v_cruise, hud_show_lanes, hud_show_car, hud_alert):
+
+    P = self.params
+
+    # Send CAN commands.
+    can_sends = []
+
+    # STEER
+    lkas_enabled = enabled and not CS.out.steerWarning and CS.out.vEgo > P.MIN_STEER_SPEED and CS.enable_lkas
+    if (frame % P.STEER_STEP) == 0:
+      if lkas_enabled:
+        new_steer = int(round(actuators.steer * P.STEER_MAX))
+        apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
+        self.steer_rate_limited = new_steer != apply_steer
+      else:
+        apply_steer = 0
+
+      self.apply_steer_last = apply_steer
+      idx = (frame // P.STEER_STEP) % 4
+
+      can_sends.append(gmcan.create_steering_control(self.packer_pt, CanBus.POWERTRAIN, apply_steer, idx, lkas_enabled))
+
+    # Pedal
+    if CS.CP.enableGasInterceptor:
+      if (frame % 2) == 0:
+        idx = (frame // 2) % 4
+
+        zero = 0.15625 * 2  #40/256
+        accel = (1 - zero) * actuators.gas + self.apply_pedal_last * zero
+        regen_brake = zero * actuators.brake
+        final_accel = accel - regen_brake
+
+        if not enabled or not CS.adaptive_Cruise:
+          final_accel = 0.
+        final_accel, self.accel_steady = accel_hysteresis(final_accel, self.accel_steady)
+        final_pedal = clip(final_accel, 0., 1.)
+        self.apply_pedal_last = final_pedal
+
+        can_sends.append(create_gas_command(self.packer_pt, final_pedal, idx))
+
+    # Send dashboard UI commands (ACC status), 25hz
+    #if (frame % 4) == 0:
+    #  send_fcw = hud_alert == VisualAlert.fcw
+    #  can_sends.append(gmcan.create_acc_dashboard_command(self.packer_pt, CanBus.POWERTRAIN, enabled, hud_v_cruise * CV.MS_TO_KPH, hud_show_car, send_fcw))
+
+    # Radar needs to know current speed and yaw rate (50hz) - Delete
+    # and that ADAS is alive (10hz)
+
+    #if frame % P.ADAS_KEEPALIVE_STEP == 0:
+    #  can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
+
+    # Show green icon when LKA torque is applied, and
+    # alarming orange icon when approaching torque limit.
+    # If not sent again, LKA icon disappears in about 5 seconds.
+    # Conveniently, sending camera message periodically also works as a keepalive.
+    lka_active = lkas_enabled == 1
+    lka_critical = lka_active and abs(actuators.steer) > 0.9
+    lka_icon_status = (lka_active, lka_critical)
+    if frame % P.CAMERA_KEEPALIVE_STEP == 0 or lka_icon_status != self.lka_icon_status_last:
+      steer_alert = hud_alert == VisualAlert.steerRequired
+      can_sends.append(gmcan.create_lka_icon_command(CanBus.SW_GMLAN, lka_active, lka_critical, steer_alert))
+      self.lka_icon_status_last = lka_icon_status
+
+    return can_sends
